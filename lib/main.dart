f69a0e8f2934f3897e7ac58c06f2165b486591f9
muhammad aziz <168031040+EYZIZ@users.noreply.github.com>
2024-10-21 13:07:58 +0700

    commit 1

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi/screen/detail_screen.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata candi',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: DetailScreen(),
+    );
+  }
+}
